d1e64f6e41bd1cd159958d04d7019903eaaedb06
Facundo Arias <facundolautaroarias@hotmail.com>
2022-04-21 01:28:05 -0300

    pages added, pre install react-chartsjs-2 all-working

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,55 +1,58 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import './CoinPage.css';
-import ReactHtmlParser from 'react-html-parser'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./CoinPage.css";
+import ReactHtmlParser from "react-html-parser";
+import { CoinGraph } from "../components/CoinGraph";
 
-const SingleCoin = (id) =>
-  `https://api.coingecko.com/api/v3/coins/${id}`;
+const SingleCoin = (id) => `https://api.coingecko.com/api/v3/coins/${id}`;
 
 export const CoinPage = () => {
   const { id } = useParams();
-  const [coin, setCoin] = useState()
-  const [coinDes, setCoinDes] = useState('')
-  const [coinMarketC, setcoinMarketC] = useState()
+  const [coin, setCoin] = useState();
+  const [coinDes, setCoinDes] = useState("");
+  const [coinMarketC, setcoinMarketC] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     axios(SingleCoin(id))
-    .then((res)=>{
-      setCoin(res.data);
-      setCoinDes(res.data.description.en);
-      setcoinMarketC(res.data.market_data.market_cap.usd)
-      console.log(res.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }, [])
-
-
+      .then((res) => {
+        setCoin(res.data);
+        setCoinDes(res.data.description.en);
+        setcoinMarketC(res.data.market_data.market_cap.usd);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
   return (
-    <div className='coin-data-container'>
-      <div className='coin-data'>
-
-        <div className='coin-card-info'>
-          <img src={coin?.image['large']} alt=''/>
-          <h2>{coin?.name}</h2>
-          <p>{ReactHtmlParser(coinDes.split(". ")[0])}</p>
-          <div className='card-market'>
-            <p><strong>Rank:</strong> {coin?.market_cap_rank}</p>
-            <p><strong>Price:</strong> {coin?.market_data.current_price.usd} USD</p>
-            <p><strong>Market Cap:</strong> {coinMarketC.toLocaleString()} USD</p>
-
+    <div className="coin-data-container">
+      <div className="single-coin-data">
+        {coin && (
+          <div className="single-coin-card-info">
+            <img src={coin?.image["large"]} alt="" />
+            <h1>{coin?.name}</h1>
+            <p>{ReactHtmlParser(coinDes.split(". ")[0])}</p>
+            <div className="card-market">
+              <p>
+                <strong>Rank:</strong> {coin?.market_cap_rank}
+              </p>
+              <p>
+                <strong>Price:</strong> {coin?.market_data.current_price.usd}{" "}
+                USD
+              </p>
+              <p>
+                <strong>Market Cap:</strong> {coinMarketC.toLocaleString()} USD
+              </p>
+            </div>
           </div>
+        )}
+
+        <div className="coin-chart">
+          <CoinGraph />
         </div>
-
-
-        <div className='coin-chart'>
-
-        </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./Homepage.css";
 import axios from "axios";
-import { Coin } from "../components/Coins";
+import { Coin } from "../components/Coin";
 
 // https://www.coingecko.com/es/api/documentation
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
